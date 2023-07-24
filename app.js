fa71e3390977cac84de21efd342fd7edac35665f
Sanjay-3797 <sanjayg.ac.in@gmail.com>
2023-07-24 23:12:49 +0530

    Request / Response

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("Sanjay");
+  const url = req.url;
+
+  if (url === "/home") {
+    res.setHeader("Context-Type", "text/html");
+    res.write("<head><title>My First Page</title></head>");
+    res.write("<html><body><h1>Welcome Home</h1></body></html>");
+    return res.end();
+  }
+
+  if (url === "/about") {
+    res.setHeader("Context-Type", "text/html");
+    res.write("<head><title>My First Page</title></head>");
+    res.write("<html><body><h1>About US Page</h1></body></html>");
+    return res.end();
+  }
+
+  if (url === "/node") {
+    res.setHeader("Context-Type", "text/html");
+    res.write("<head><title>My First Page</title></head>");
+    res.write(
+      "<html><body><h1>Welcome to my Node Js project</h1></body></html>"
+    );
+    return res.end();
+  }
 });
 
-server.listen(4000);
+server.listen(3000);
