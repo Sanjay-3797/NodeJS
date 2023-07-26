ee78a29a8312d492e367a2d4d1c79ba58bf40d97
Sanjay-3797 <sanjayg.ac.in@gmail.com>
2023-07-26 12:24:57 +0530

    Clean up code

--- app.js
+++ app.js
@@ -1,30 +1,7 @@
 const http = require("http");
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
+const routes = require("./routes");
 
-  if (url === "/home") {
-    res.setHeader("Context-Type", "text/html");
-    res.write("<head><title>My First Page</title></head>");
-    res.write("<html><body><h1>Welcome Home</h1></body></html>");
-    return res.end();
-  }
-
-  if (url === "/about") {
-    res.setHeader("Context-Type", "text/html");
-    res.write("<head><title>My First Page</title></head>");
-    res.write("<html><body><h1>About US Page</h1></body></html>");
-    return res.end();
-  }
-
-  if (url === "/node") {
-    res.setHeader("Context-Type", "text/html");
-    res.write("<head><title>My First Page</title></head>");
-    res.write(
-      "<html><body><h1>Welcome to my Node Js project</h1></body></html>"
-    );
-    return res.end();
-  }
-});
+const server = http.createServer(routes);
 
 server.listen(3000);
