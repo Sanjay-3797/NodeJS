9a1772a3f69011ef3f4f6dd076386b5746e20439
Sanjay-3797 <sanjayg.ac.in@gmail.com>
2023-07-27 18:05:49 +0530

    Middlewares

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-const http = require("http");
-
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("First Middleware");
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title'/><input type='number' name='size'/><button type='submit'>Add Product</button></form>"
+  );
 });
 
-app.use((req, res, next) => {
-  console.log("Another Middleware");
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
-const server = http.createServer(app);
+app.use("/", (req, res, next) => {
+  res.send("<h1>Express Js</h1>");
+});
 
-server.listen(3000);
+app.listen(3000);
