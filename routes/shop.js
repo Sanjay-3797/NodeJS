fd15c3249baf01243670375d79adad16bfa4089e
Sanjay-3797 <sanjayg.ac.in@gmail.com>
2023-07-29 14:35:29 +0530

    HTML , CSS and JS

--- routes/shop.js
+++ routes/shop.js
@@ -2,8 +2,12 @@ const express = require("express");
 
 const router = express.Router();
 
+const rootDir = require("../util/path");
+
+const path = require("path");
+
 router.get("/", (req, res, next) => {
-  res.send("<h1>Express Js</h1>");
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
 module.exports = router;
